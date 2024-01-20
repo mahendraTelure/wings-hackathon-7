99b7a74f1f612164398b63b88a2af533522bec3e
Mahendra Telure <teluremahendra@gmail.com>
2024-01-20 09:17:11 +0530

    setup commit

---
+++ BeyondLimitsEduServer/src/main/java/com/wingshackathon7/BeyondLimitsEdu/BeyondLimitsEduApplication.java
@@ -0,0 +1,13 @@
+package com.wingshackathon7.BeyondLimitsEdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondLimitsEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondLimitsEduApplication.class, args);
+	}
+
+}
---
+++ BeyondLimitsEduServer/src/test/java/com/wingshackathon7/BeyondLimitsEdu/BeyondLimitsEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wingshackathon7.BeyondLimitsEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondLimitsEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
